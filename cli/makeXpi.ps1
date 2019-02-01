fdd74c43fd60ec2b86059a910e92d3a38c95704e
jamaisx <jamaisx@gmail.com>
2019-02-01 16:07:26 +0100

    script improperly include src folder into xpi file, instead of compress only contained files

--- cli/makeXpi.ps1
+++ cli/makeXpi.ps1
@@ -5,7 +5,7 @@ Compress-Archive -Force .\chrome messagenotesplus
 move .\messagenotesplus.zip .\chrome\messagenotesplus.jar
 
 cd ..
-Compress-Archive -Force .\src src1
+Compress-Archive -Force .\src\* src1
 rm distr\message_notes_plus-1.5.1.0.xpi -ErrorAction Ignore
 move src1.zip distr\message_notes_plus-1.5.1.0.xpi
 popd
